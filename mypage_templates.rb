4b16611c22f3cbc4180b8e7c8cd70423592cfa2b
harukasan <harukasan@pixiv.com>
2014-09-28 09:00:19 +0000

    fix bug

--- mypage_templates.rb
+++ mypage_templates.rb
@@ -9,7 +9,7 @@ def gen_mypage(last_login)
 <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
 <title>isucon4</title>
 </head>
-<body data-ip="#{last_login["ip"]}" data-at="#{last_login["created_at"]}">
+<body data-ip="#{last_login["ip"]}" data-last="#{last_login["created_at"]}">
 <script type="text/template" id="last-logined-at">#{last_login["created_at"]}</script>
 <script type="text/template" id="last-logined-ip">#{last_login["ip"]}</script>
 <script src="/mypage.js" type="text/javascript"></script>
